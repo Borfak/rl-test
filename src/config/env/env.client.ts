3514ec4e635bfa644993ab16418f1a4cafa62bea
Borfak <borfak@protonmail.com>
2025-10-24 19:28:21 +0300

    fix: hotfix

--- src/config/env/env.client.ts
+++ src/config/env/env.client.ts
@@ -4,10 +4,13 @@ import { createEnv } from '@t3-oss/env-nextjs'
 // env client
 export const envClient = createEnv({
   client: {
-    NEXT_PUBLIC_APP_URL: z.url().optional().default('http://localhost:3000'),
+    NEXT_PUBLIC_APP_URL: z.string().url().optional(),
   },
   emptyStringAsUndefined: true,
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 
+      (typeof window !== 'undefined' ? window.location.origin : 
+        process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 
+        'http://localhost:3000'),
   },
 })
