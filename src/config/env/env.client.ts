6f8dac182f7e60ce27aed62dab8c34ff5be4d70b
Borfak <borfak@protonmail.com>
2025-10-24 19:41:08 +0300

    fix: env client fix

--- src/config/env/env.client.ts
+++ src/config/env/env.client.ts
@@ -4,13 +4,10 @@ import { createEnv } from '@t3-oss/env-nextjs'
 // env client
 export const envClient = createEnv({
   client: {
-    NEXT_PUBLIC_APP_URL: z.string().url().optional(),
+    NEXT_PUBLIC_APP_URL: z.url().optional(),
   },
   emptyStringAsUndefined: true,
   runtimeEnv: {
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 
-      (typeof window !== 'undefined' ? window.location.origin : 
-        process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 
-        'http://localhost:3000'),
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
